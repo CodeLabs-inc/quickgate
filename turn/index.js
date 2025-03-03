0850d70d4ccd6c3c60761da58059e7c05a644519
federico <federicoaugusto.lacchini@gmail.com>
2025-03-03 20:27:40 +0100

    turn server

---
+++ turn/index.js
@@ -0,0 +1,9 @@
+const { PeerServer } = require('peer');
+
+const peerServer = PeerServer({
+  port: 9000, // Open this port in AWS Security Group
+  path: '/calls',
+  proxied: true
+});
+
+console.log("PeerJS server running on port 9000");
